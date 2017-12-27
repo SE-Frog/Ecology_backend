3cf59aadd0323cb430251c2bc663e47501999ccd
mikesam9565 <samson9565@gmail.com>
2017-12-28 00:58:59 +0800

    增加deleteEcology功能，刪除所點擊的哪一筆資料

--- Control/SearchControl.php
+++ Control/SearchControl.php
@@ -1,4 +1,16 @@
 <?php
-    session_start();
+  session_start();
+  require_once('../Modules/Search.php');
+  $action =$_REQUEST['act'];
 
+  switch ($action) {
+    case 'deleteEcology':
+      $id = (int) $_REQUEST['id'];
+      if ($id > 0) {
+        // // if ($id == $_SESSION['uID']){
+        deleteEcology($id);
+      }
+      break;
+}
 ?>
+<?php header('Location: ../View/SearchView.php'); ?>
