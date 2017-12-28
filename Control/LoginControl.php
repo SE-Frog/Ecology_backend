5c5b6c629bc0888f726c5789172b0eaea876632f
TMineCola <s104213083@mail1.ncnu.edu.tw>
2017-12-28 09:28:29 +0800

    Prettify

--- Control/LoginControl.php
+++ Control/LoginControl.php
@@ -1,22 +1,26 @@
 <?php
-session_start(); //啟用session
-require_once('../Modules/loginModel.php');
-$action =$_REQUEST['act'];
+    // 啟用session
+    session_start();
+    // 引入loginModel
+    require_once('../Modules/loginModel.php');
+    $action =$_REQUEST['act'];
 
-switch ($action) {
-case 'login':
-	$userName = $_POST['id']; //取得從HTML表單傳來之POST參數
-	$passWord = $_POST['pwd'];
-	
-	if ($id=checkIdentity($userName,$passWord)) { //比對密碼
-      $_SESSION['uID'] = $id; //若正確－－＞將userID存在session變數中，作為登入成功之記號
-			header('Location: ../Views/index.php');
-	} else {
-		//print error message
-		echo "Invalid Username or Password - Please try again <br />";
-		echo '<a href="../Views/loginForm.php">Login again</a> ';
-	}
-	break;
+    switch ($action) {
+        case 'login':
+            // 取得從HTML表單傳來之POST參數
+            $userName = $_REQUEST['id'];
+            $passWord = $_REQUEST['pwd'];
+            // 比對密碼
+            if ( $id == checkIdentity($userName,$passWord)) {
+                //若正確則將userID存在session變數中，作為登入成功之記號，並導回首頁
+                $_SESSION['uID'] = $id;
+                header('Location: ../Views/index.php');
+            } else {
+                //print error message
+                echo "Invalid Username or Password - Please try again <br />";
+                echo '<a href="../Views/loginForm.php">Login again</a> ';
+            }
+        break;
 
-}
+    }
 ?>
