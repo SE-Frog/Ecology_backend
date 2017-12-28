f8cdf3924090d19ebe38bc79d5532c28b46e8cf8
mikesam9565 <samson9565@gmail.com>
2017-12-28 14:41:51 +0800

    header and footer化

--- Views/header.php
+++ Views/header.php
@@ -1,13 +1,11 @@
-
+<!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" />
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"/>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"/> -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <nav class="navbar navbar-default">
   <div class="container">
@@ -27,19 +25,21 @@
         <!-- <li class="active"><a href="#"> <span class="sr-only">(current)</span></a></li> -->
         <li><a href="../Views/index.php">首頁</a></li>
         <li><a href="../Views/userinfo.php">使用者清單</a></li>
+        <li><a href="../Views/SearchView.php">查看資料庫</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-      <li><a href='loginForm.php'>
-        <?php 
-        session_start();
-        if (isset($_SESSION['uID']) && $_SESSION['uID'] == true) {
-          echo "登出";
-        } else {
-          echo "登錄";
-        }
-        ?>
+        <li><a href='loginForm.php'>
+          <?php 
+          session_start();
+          if (isset($_SESSION['uID']) && $_SESSION['uID'] == true) {
+            echo "登出";
+          } else {
+            echo "登錄";
+          }
+          ?>
         </a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div>
 </nav>
+<body>
