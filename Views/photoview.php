bfd89c1f7d26835ef4b97fbdf240a7013c6b288a
mikesam9565 <samson9565@gmail.com>
2018-01-05 23:27:42 +0800

    從資料庫內撈出所有資源顯示出來

---
+++ Views/photoview.php
@@ -0,0 +1,105 @@
+<?php
+  include 'header.php';
+  if ( ! isset($_SESSION['uID']) or $_SESSION['uID'] <= 0) {
+    header("Location: ../Views/loginForm.php");
+    exit(0);
+  }
+?>
+	<title>EXIF 信息</title>
+	<script type="text/javascript">
+		function select_format(){
+			var on=document.getElementById('fmt').checked;
+			document.getElementById('site').style.display=on?'none':'';
+			document.getElementById('sited').style.display=!on?'none':'';
+		};
+		var flag=false;
+		function DrawImage(ImgD){
+			var image=new Image();
+			image.src=ImgD.src;
+			if(image.width>0&&image.height>0){flag=true;
+				if(image.width/image.height>=120/80){
+					if(image.width>120){
+						ImgD.width=120;ImgD.height=(image.height*120)/image.width;
+					}else {
+						ImgD.width=image.width;
+						ImgD.height=image.height;
+					};
+					ImgD.alt=image.width+"×"+image.height;
+				} else {
+					if(image.height>80){
+					ImgD.height=80;
+					ImgD.width=(image.width*80)/image.height;
+					} else {
+					ImgD.width=image.width;
+					ImgD.height=image.height;
+					};
+				ImgD.alt=image.width+"×"+image.height;
+				}
+			};
+		};
+		function FileChange(Value){
+			flag=false;
+			document.all.uploadimage.width=10;
+			document.all.uploadimage.height=10;
+			document.all.uploadimage.alt="";
+			document.all.uploadimage.src=Value;
+		};
+	</script>
+	<style type="text/css">
+		body {
+			padding:0;
+			margin:0;
+			font-size:12px;
+		}
+
+		#wrapper {
+			width:960px;
+			margin:50px auto;
+			min-height:400px;
+		}
+
+		#image {
+			float:left;
+		}
+
+		#backder {
+			border:1px solid #CCC;
+			padding:5px;
+			margin-bottom:5px;
+		}
+
+		#imageMeta {
+			margin:0 0 0 10px;
+			float:left;
+		}
+		</style>
+
+    
+    <div id="wrapper">
+      <?php
+      require('../Modules/Function.php');
+  		$result=getPhotoExif();
+      while (	$rs=mysqli_fetch_array($result)) {
+        echo " <div id=image>";
+          echo " <div id=backder>";
+          echo " <a href=".$rs['path']." target='_blank'><img src=\"".$rs['path']."\" id=\"img\" exif=\"true\" width=".(500)." height=".(500);
+          echo " title=\"信息:\r文件名:".$rs['name']."\r上傳時間:".$rs['createtime']."\" border='0'></a>";
+          echo " </div>";
+        echo " </div>";
+        echo " <div id=imageMeta>";
+        echo " <div class=exif-data>";
+          echo " <b>圖片名稱：".$rs['name']."</b><br/><br/>";
+          echo " <b>圖片路徑：".$rs['path']."</b><br/><br/>",
+  						  "<b>經度：</b>".$rs['longitude']."<br><br>",
+  						  "<b>緯度：</b>".$rs['latitude']."<br><br>",
+  						  "<b>拍攝日期：".$rs['shootdatetime']."</b>";
+          echo " </div>";
+        echo " <br>";
+        echo " </div>";
+      }
+		?>
+    </div>
+    
+<?php
+  include 'footer.php';
+?>
