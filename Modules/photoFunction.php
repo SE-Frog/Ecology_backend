b36b8d46b5a7b8b4eb93827b17e6595eca8216a3
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-06 11:07:21 +0800

    新增Read部分函式

---
+++ Modules/photoFunction.php
@@ -0,0 +1,61 @@
+<?php
+    // 將MySQL連線資訊從dbConnect.php載入
+    require_once('dbConnect.php');
+
+    /* 獲取全部生態照片庫資訊 */
+    function getFullPhoto() {
+        // 宣告使用conn全域變數
+        global $conn;
+        // 選取photo資料表中所有照片資訊
+        $sql = "SELECT * FROM `photo`";
+
+        return mysqli_query($conn, $sql);
+    }
+
+    /* 獲得指定編號生態照片資訊 */
+    function getPhotoID($id) {
+        // 宣告使用conn全域變數
+        global $conn;
+        // 針對id做基本檢誤
+        $id = (int)$id;
+        // 選取photo資料表中所有照片資訊
+        $sql = "SELECT * FROM `photo` WHERE `id` = $id";
+
+        return mysqli_query($conn, $sql);
+    }
+
+    /* 獲得指定生物(資料夾)生態照片資訊 */
+    function getPhotoDirectory($directory) {
+        // 宣告使用conn全域變數
+        global $conn;
+        // 字串過濾
+        $directory = mysqli_real_escape_string($conn, $directory);
+        // 選取photo資料表中所有照片資訊
+        $sql = "SELECT * FROM `photo` WHERE `directory` = $directory";
+
+        return mysqli_query($conn, $sql);
+    }
+
+    /* 獲得目前所有資料夾 (可用於搜尋時的Option顯示) */
+    function getDirectory() {
+        // 宣告使用conn全域變數
+        global $conn;
+        // 選取並用標籤進行分群
+        $sql = "SELECT `directory` FROM `photo` GROUP BY `directory`";
+
+        return mysqli_query($conn, $sql);
+    }
+
+    function searchPhoto($directory = '%', $name = '%') {
+        // 宣告使用conn全域變數
+        global $conn;
+        // 過濾字串, 並針對有值參數進行修改
+        if($directory != "%") {
+            $directory = "%" . mysqli_real_escape_string($conn, $directory) . "%";
+        }
+        if($name != "%") {
+            $name = "%" . mysqli_real_escape_string($conn, $name) . "%";
+        }
+        $sql = "SELECT * FROM `photo` WHERE (`directory` LIKE '$directory' OR `directory` IS NULL) AND `name` LIKE '$name'";
+    }
+?>
