d3a81ce6cae844f3a060f15db4db562dcbd66f1f
mikesam9565 <samson9565@gmail.com>
2018-01-07 03:22:51 +0800

    如果checked要複寫，overwrite =1，產生表單。
    未完待續

--- Views/photoedit.php
+++ Views/photoedit.php
@@ -21,7 +21,7 @@
     exit(0);
   }
 ?>
-
+<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
 
 <div class="container">
 <?php
@@ -45,6 +45,8 @@
     <b>經度: </b><input name="longitude" type="text" id="longitude" value="<?php echo $longitude;?>" /> <br>
     <b>緯度: </b><input name="latitude" type="text" id="latitude" value="<?php echo $latitude;?>" /> <br>
     <b>拍攝日期: </b><input name="shootdatetime" type="text" id="shootdatetime" value="<?php echo $shootdatetime;?>" /> <br>
+    <b>更改圖片？</b><input name="overwrite" value="1" id="overwrite" class="checkbox_check" type="checkbox" />
+    <b id="overwrite1"></b>
     <input type="submit" name="Submit" value="送出" />[<a href='../Views/photoview.php'>返回</a>]
 <?php
   echo "</form>";
@@ -58,3 +60,19 @@
 <?php   
   include 'footer.php';
 ?>
+<script type="text/javascript">
+// console.log('1');
+$(document).ready(function () {
+  $("input[name='overwrite']").change(function () {
+    var $this = $(this);
+    if ( $this.prop('checked') ) {
+      var option = $(this).val();
+      $(this).parent().append("<input id='upload' class='form-control' name='exif[]' type='file' multiple='multiple' placeholder='可選擇多檔案上傳'/>");
+    } else {
+      if($("body").find("#upload")){
+        $('#upload').remove();
+      }
+    }
+  });
+});
+</script>
